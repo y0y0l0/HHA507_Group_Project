7a64e3325818b81d5071f1ed4e47be8e4252928f
yolo <224895660+y0y0l0@users.noreply.github.com>
2025-12-03 23:23:25 -0500

    fix functions

--- scripts/part4-flags.py
+++ scripts/part4-flags.py
@@ -1,5 +1,5 @@
-
-from scripts.common_clean_functions import get_athletes_not_tested_in_last_num_days
+import platform as platform
+from common_clean_functions import get_athletes_not_tested_in_last_num_days
 
 ##4.1 Identify athletes who haven't been tested in the last 30 days(for your selected metrics)
 get_athletes_not_tested_in_last_num_days(30,"4.1")
